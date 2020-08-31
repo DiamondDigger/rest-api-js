f3fa34b0d4abb9256d18aec60e4acd21ac0233ad
naglec13 <naglec13@gmail.com>
2020-08-31 11:04:30 +0300

    added in app.js package 'client' as static via express for app server to load client from app.js(another folder)

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
-
+const path = require('path')
 const app = express()
 
-app.listen(3000, ()=> console.log('Server has been started on port 3000... '))
+app.use(express.static(path.resolve(__dirname, 'client')))
+
+app.get('*', (res, req) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
+})
+app.listen(3000, () => console.log('Server has been started on port 3000... '))
 
